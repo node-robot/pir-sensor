0ea750ac541f32dd66492f5a0109acd8752a7a4f
Chyld Medford <chyld.medford@gmail.com>
2014-06-17 22:35:07 -0500

    done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var prompt = require('prompt');
 var chalk  = require('chalk');
 var sp     = require('serialport');
+var cp     = require('child_process');
+var _      = require('lodash');
 
 var SerialPort = sp.SerialPort;
 var serialPort = new SerialPort('/dev/tty.usbmodem1451', {
@@ -17,13 +19,24 @@ serialPort.on('open', function(){
 
 serialPort.on('data', function(data) {
   console.log(chalk.blue.bold('Rx'), chalk.green.underline(data));
-  command();
+
+  var isPir = data.indexOf("PIR") !== -1;
+  if(isPir){
+    var value = data.split(':')[1] * 1;
+    if(value){lazyWarning();}
+  }else{
+    command();
+  }
 });
 
 serialPort.on('close', function(){
   console.log(chalk.red('Serial Port : Closed'));
   process.exit();
 });
+
+function warning(){
+  cp.exec('say warning');
+}
 
 function command(){
   prompt.get(['arduino'], function (err, result) {
@@ -35,3 +48,4 @@ function command(){
 }
 
 serialPort.open();
+var lazyWarning = _.debounce(warning, 2000, {leading:true, trailing:false});
--- arduino.ino
+++ arduino.ino
@@ -1,18 +1,29 @@
 char buffer[100];
+int pirPin = 7;
+int pirVal;
 
 void setup(){
+  pinMode(pirPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop(){
   if(Serial.available()){
+    memset(buffer, 0, sizeof(buffer));
     int b = Serial.readBytesUntil('\n', buffer, 100);
     Serial.print("Found: ");
     Serial.print(b);
     Serial.print("; Chars: ");
     Serial.println(buffer);
+  }
 
-    memset(buffer, 0, sizeof(buffer));
+  int isPir = strcmp(buffer, "pir");
+
+  if(isPir == 0){
+    pirVal = digitalRead(pirPin);
+    Serial.print("PIR: ");
+    Serial.println(pirVal);
+    delay(250);
   }
 
   delay(10);
